d645cc8cee64371e3c7402fb8d3ea0f1a9110db3
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-08-17 02:03:41 -0400

    working on the navigation bar on the side used to filter sports

--- app/src/main/java/org/ohmstheresistance/gameonandroidchallenge/MainActivity.java
+++ app/src/main/java/org/ohmstheresistance/gameonandroidchallenge/MainActivity.java
@@ -1,6 +1,5 @@
 package org.ohmstheresistance.gameonandroidchallenge;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +11,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import org.ohmstheresistance.gameonandroidchallenge.model.Games;
 import org.ohmstheresistance.gameonandroidchallenge.network.GameService;
@@ -31,9 +31,11 @@ import retrofit2.Retrofit;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "GameJSON.TAG";
+    private NavigationView sports_navView;
     private RecyclerView gamesRecyclerView;
     private DrawerLayout drawerLayout;
     private NavigationView navView;
+    private Button referFriendButton;
 
     private List<Games> gamesList;
 
@@ -42,8 +44,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout =  findViewById(R.id.drawerLayout);
-        navView =  findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navView = findViewById(R.id.nav_view);
+        referFriendButton = findViewById(R.id.refer_friend_button);
+        sports_navView = findViewById(R.id.sports_nav_view);
+
+
+        sports_navView.getMenu().getItem(0).setChecked(true);
+
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -93,8 +101,8 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "Retrofit call works " + response.body().get(6).getBroadcast_info());
 
                 GamesAdapter gamesAdapter = new GamesAdapter(gamesList);
-                    gamesRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                    gamesRecyclerView.setAdapter(gamesAdapter);
+                gamesRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                gamesRecyclerView.setAdapter(gamesAdapter);
 
 
             }
@@ -108,16 +116,45 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        sports_navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                menuItem.setChecked(true);
+
+                switch (menuItem.getItemId()) {
+                    case R.id.sports_navigation_all:
+                        break;
+                    case R.id.sports_navigation_mlb:
+                        break;
+                    case R.id.sports_navigation_nfl:
+                        break;
+                    case R.id.sports_navigation_nba:
+                        break;
+                    case R.id.sports_navigation_epl:
+                        break;
+                }
+                return true;
+
+
+            }
+
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
 
 }
